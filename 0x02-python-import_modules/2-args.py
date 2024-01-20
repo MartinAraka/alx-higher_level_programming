ab4ec0742a7f63de6735122d8890cd1310432ed2
Martin Araka <arakamartin@gmail.com>
2024-01-20 14:45:21 -0800

    Count arguements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,14 @@
 
 if __name__ == "__main__":
 
-    import sys
+    import sys as s
 
     count = len(s.argv) - 1
     if count == 0:
-        print("0 arguements.")
+        print("0 arguments.")
     elif count == 1:
-        print("1 arguement:")
+        print("1 argument:")
     else:
-        print("{} arguements:".format(count))
+        print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, s.argv[i + 1]))
