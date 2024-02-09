d511f5e96ceb4861b99b99b35e33a025914443df
Martin Araka <arakamartin@gmail.com>
2024-02-09 03:16:25 -0800

    Cuurent sqare area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""A class defining a square"""
+
+
+class Square:
+    """A class defining a square"""
+    def __init__(self, size=0)
+    """
+
+    Constructs a square
+    Args:
+    size (int): The side of the square
+    """
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
+
+
+def area(self):
+    """Returns the area of the square"""
+    return self.__size ** 2
