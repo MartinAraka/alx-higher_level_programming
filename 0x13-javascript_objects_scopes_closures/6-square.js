1bae37616d1873c769390af84cf951446b9c944d
Martin Araka <arakamartin@gmail.com>
2024-04-24 14:33:31 -0700

    Create instance method chaPrint

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Square1 = require('./5-square.js');
 
-class Square extends Rectangle  {
-  constructor  (size)  {
-    super(size, sie);
-  }
-
-  charPrint  (c)  {
-    if  (c === undefine)  this.print();
-    else  {
-      for  (let i = 0; i < this.height; i++)  console.log(c.repeat(this.width));
+class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    let i = 0;
+    for (i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
     }
   }
 }
+
+module.exports = Square;
