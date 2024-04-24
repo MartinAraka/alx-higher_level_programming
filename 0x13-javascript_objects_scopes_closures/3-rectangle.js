adbd6eda7065b0cfba7780f672335ecb2e3d256d
Martin Araka <arakamartin@gmail.com>
2024-04-24 12:09:02 -0700

    Create instance method print

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,24 +1,22 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-    constructor (w, h) {
-      
-      if (w <= 0 || h <= 0) { 
-        new Rectangle()
-      } else{
-        this.width = w
-        this.height = h
-      }
-    }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        let shape = ""
-        for (let j = 0; j < this.width; j++) {
-          shape += "X";
-        }
-        console.log(shape)
-      }
 
-      console.log()
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !w || !h) {
+      return;
     }
-  };
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let iter = 0; iter < this.height; iter++) {
+      let row = '';
+      for (let iter2 = 0; iter2 < this.width; iter2++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Rectangle;
