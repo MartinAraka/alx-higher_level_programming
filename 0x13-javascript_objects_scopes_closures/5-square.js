b6a2d2d3f1f60a2449a3f5ba9252172a6184b64b
Martin Araka <arakamartin@gmail.com>
2024-04-23 19:22:23 -0700

    Write class Square

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+	constructor (size) {
+		super(sie, size);
+	}
+}
+
+module.exports = Square;
+
